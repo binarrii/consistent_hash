00f7d031ed8c3e6cdc2bc209697d600be5e514a4
dupp <dupp@wasu.com>
2022-04-19 19:57:20 +0800

    initial import

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::os::raw::{c_long, c_int};
+
+struct LinearCongruentialGenerator {
+    state: i64,
+}
+
+impl LinearCongruentialGenerator {
+    pub fn next_double(&mut self) -> f64 {
+        self.state = 2862933555777941757i64.wrapping_mul(self.state) + 1;
+        ((((((self.state as u64) >> 33) as i64) as i32) + 1) as f64) / (2.0f64).powi(31)
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn consistent_hash(input: c_long, buckets: c_int) -> c_int {
+    let mut generator = LinearCongruentialGenerator { state: input };
+
+    let mut candidate: i32 = 0;
+    let mut next: i32;
+    loop {
+        next = (((candidate + 1) as f64) / generator.next_double()) as i32;
+        if next >= 0 && next < buckets {
+            candidate = next;
+        } else {
+            return candidate;
+        }
+    }
+}
